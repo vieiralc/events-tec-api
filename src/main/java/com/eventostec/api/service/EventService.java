98f0c80b25fe7a6ce19a9b2c9be6ee1f806fd566
Lucas Vieira <vieira.lucasmm@gmail.com>
2024-11-11 21:33:30 -0300

    feat: add event controller and update event service to upload file to aws

--- src/main/java/com/eventostec/api/service/EventService.java
+++ src/main/java/com/eventostec/api/service/EventService.java
@@ -1,14 +1,31 @@
 package com.eventostec.api.service;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
 
 @Service
 public class EventService {
+
+    @Value("${aws.bucket.name}")
+    private String bucketName;
+
+    @Autowired
+    private AmazonS3 s3Client;
+    @Autowired
+    private EventRepository eventRepository;
+
     public Event createEvent(EventRequestDTO data) {
         String imgUrl = null;
 
@@ -21,13 +38,37 @@ public class EventService {
         newEvent.setDescription(data.description());
         newEvent.setEventUrl(data.eventUrl());
         newEvent.setDate(new Date(data.date()));
-        newEvent.setImgUrl(data.eventUrl());
+        newEvent.setImgUrl(imgUrl);
+        newEvent.setRemote(data.remote());
+
+        eventRepository.save(newEvent);
 
         return newEvent;
     }
 
-    private String uploadImage(MultipartFile file) {
-        System.out.println(file);
-        return "";
+    private String uploadImage(MultipartFile multipartFile) {
+        String fileName = this.generateUniqueImgName(multipartFile.getOriginalFilename());
+
+        try {
+            File file = this.convertMultiPartToFile(multipartFile);
+            s3Client.putObject(bucketName, fileName, file);
+            file.delete();
+            return  s3Client.getUrl(bucketName, fileName).toString();
+        } catch (Exception e) {
+            System.out.println("error uploading file: " + e);
+            return "";
+        }
+    }
+
+    private String generateUniqueImgName(String originalFileName) {
+        return UUID.randomUUID() + " " + originalFileName;
+    }
+
+    private File convertMultiPartToFile(MultipartFile file) throws Exception {
+        File convFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convFile;
     }
 }
